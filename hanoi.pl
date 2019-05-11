d283183c024e571b3641c1a1298e8922a3df8b36
Mohamed <elraghy8@gmail.com>
2019-05-12 01:17:24 +0200

    Tower of hanoi

---
+++ hanoi.pl
@@ -0,0 +1,15 @@
+hanoi(N) :-
+    move(N, left, centre, right).
+
+move(0, _, _, _) :-
+    !.
+
+move(N, A, B, C) :-
+    M is N-1,
+    move(M, A, C, B),
+    inform(A, B),
+    move(M, C, B, A).
+
+inform(X, Y) :-
+    write([move, a, disc, from, the, X, pole, to, the, Y, pole]),
+    nl.
